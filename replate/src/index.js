e44a268958747b0f09b33fecb1837c8044e38acf
Tabita Filimon <tabitafilimon@gmail.com>
2019-03-18 15:05:26 -0400

    built out reducer function

--- replate/src/index.js
+++ replate/src/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+import reducer from './reducers';
+
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+
+const store = createStore(reducer, applyMiddleware(thunk, logger));
+
+ReactDOM.render(
+<Provider store = {store}>
+<App />
+</Provider>
+, document.getElementById('root'));
 
